c30c22d661db2b41c18340ffb53fbde0eda1ab3c
BinanceAPI-at-058264333313 <a.khlapuk@bankvostok.com.ua>
2024-04-06 19:28:52 +0300

    Connect to Db and create simple Controller for Categories

---
+++ BooksShopWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BooksShopWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksShopWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
