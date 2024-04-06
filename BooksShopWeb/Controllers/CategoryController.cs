b12027a1186e2a8be2dc19e1ce89965551f3a6a6
ArturCode <a.khlapuk@bankvostok.com.ua>
2024-04-07 00:33:37 +0300

    Change project style and add Create Category page

--- BooksShopWeb/Controllers/CategoryController.cs
+++ BooksShopWeb/Controllers/CategoryController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BooksShopWeb.Data;
+using BooksShopWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BooksShopWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
