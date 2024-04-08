5af267ce4b6fc012465b012b725cdfdc3d846859
ArturCode <a.khlapuk@bankvostok.com.ua>
2024-04-08 22:05:53 +0300

    Add Update and Remove Category methods and views

--- BooksShopWeb/Controllers/CategoryController.cs
+++ BooksShopWeb/Controllers/CategoryController.cs
@@ -39,5 +39,58 @@ namespace BooksShopWeb.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromdb = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryFromdb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromdb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromdb = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryFromdb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromdb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category categoryToDelete = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryToDelete == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(categoryToDelete);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
